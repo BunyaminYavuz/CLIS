739fc695d4292d9f5eb8b87ec8374ede563bd6e5
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-10-08 19:35:35 +0300

    Setup Database Connection with MongoDB Atlas and Mongoose using ENV
    - Established MongoDB Atlas connection via Mongoose
    - Configured environment variables for secure DB connection
    - Updated connection logic to utilize ENV settings for flexibility

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from "express"
+import dotenv from "dotenv"
+import conn from "./db.js"
+
+dotenv.config()
+
+// db connection 
+conn()
 
 const app = express();
-
-const port = 3000;
+const port = process.env.PORT;
 
 // ejs template engine
 app.set("view engine", "ejs")
