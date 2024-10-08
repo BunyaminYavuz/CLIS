ced69b0e595a85f13db6cc22510769700a0e4075
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-10-08 14:21:04 +0300

    Project setup

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express();
+
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    res.send("Index Page");
+})
+
+app.listen(port, ()=>{
+    console.log(`Application is running on ${port}`);
+});
