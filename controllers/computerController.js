15ea1cd929a89b28449a22588c1b14af67b836b9
Bunyamin Yavuz <1b.yavuz07@gmail.com>
2024-10-09 11:02:31 +0300

    Utilize Thunder Client for API Testing with Document and Collection
    - Set up requests and responses using Thunder Client for API testing
    - Created and organized document collections for efficient request management
    - Verified API responses for proper functionality

--- controllers/computerController.js
+++ controllers/computerController.js
@@ -1,12 +1,37 @@
-import Computer from "../models/computerModel";
+import Computer from "../models/computerModel.js";
 
-const createComputer = (req, res) => {
-    const computer = Computer.create(req.body);
+const createComputer = async (req, res) => {
+  try {
+    const computer = await Computer.create(req.body);
     res.status(201).json({
-        succeded : true,
-        computer
+      succeded: true,
+      computer,
+    });
+  } catch (error) {
+    res.status(500).json({
+        succeded : false,
+        error
     })
-}
+  }
+};
 
 
-export { createComputer }
+const getAllComputers = async (req, res) => {
+    try {
+      const computers = await Computer.find({});
+      res.status(200).json({
+        succeded: true,
+        computers,
+      });
+    } catch (error) {
+      res.status(500).json({
+          succeded : false,
+          error
+      })
+    }
+  };
+
+
+
+
+export { createComputer, getAllComputers };
--- routes/computerRoute.js
+++ routes/computerRoute.js
@@ -4,6 +4,7 @@ import * as computerContorller from "../controllers/computerController.js"
 const router = express.Router();
 
 router.route("/").post(computerContorller.createComputer)
+router.route("/").get(computerContorller.getAllComputers)
 
 
 export default router;
